4e6debf4ef985925f61e1e98ef48740c2b62fb6e
badgujarankush <ankush.badgujar58@gmail.com>
2021-08-18 16:25:41 +0530

    infoBox text changed

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -6,16 +6,27 @@ function InfoBox({title,cases,total}) {
         <div>
             <Card className="infoBox" style={{ backgroundColor: "#011c29"}}>
                 <CardContent >
-                    {/* Title Corona cases */}
+                    {/* Title Corona cases
                     <Typography className="infoBox_title" >
                         {title}
                     </Typography>
 
                     {/* Today cases +48k */}
+                    {/* <h2 className="infoBox_cases" >{cases}</h2> */}
+
+                    {/* Total cases : 1.2M*/}
+                    {/* <Typography className="infoBox_total" >{total} Total</Typography> */}
+
+                    {/* Title Corona cases */}
+                    <h4 className="infoBox_title" >
+                        {title}
+                    </h4>
+
+                    {/* Today cases +48k */}
                     <h2 className="infoBox_cases" >{cases}</h2>
 
                     {/* Total cases : 1.2M*/}
-                    <Typography className="infoBox_total" >{total} Total</Typography>
+                    <h4 className="infoBox_total" >{total} Total</h4>
                 </CardContent>
             </Card>
         </div>
