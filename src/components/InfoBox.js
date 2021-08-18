649dfdd1992f0551ef828a0a8ca512933ab9099e
badgujarankush <ankush.badgujar58@gmail.com>
2021-08-18 16:30:34 +0530

    changes to infobox

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import {Card, CardContent,Typography} from "@material-ui/core"
+import {Card, CardContent} from "@material-ui/core"
 import './InfoBox.css'
 function InfoBox({title,cases,total}) {
     return (
         <div>
             <Card className="infoBox" style={{ backgroundColor: "#011c29"}}>
                 <CardContent >
-                    {/* Title Corona cases
-                    <Typography className="infoBox_title" >
-                        {title}
-                    </Typography>
-
-                    {/* Today cases +48k */}
-                    {/* <h2 className="infoBox_cases" >{cases}</h2> */}
-
-                    {/* Total cases : 1.2M*/}
-                    {/* <Typography className="infoBox_total" >{total} Total</Typography> */}
 
                     {/* Title Corona cases */}
                     <h4 className="infoBox_title" >
